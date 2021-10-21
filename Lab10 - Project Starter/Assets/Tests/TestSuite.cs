30a050c9a70e70585e5845b7d229d5f3972dea30
15ShermanWoo <shermanwwj@hotmail.com>
2021-10-22 01:02:34 +0800

    Update TestSuite.cs
    Added more Codes

--- Lab10 - Project Starter/Assets/Tests/TestSuite.cs
+++ Lab10 - Project Starter/Assets/Tests/TestSuite.cs
@@ -26,5 +26,99 @@ namespace Tests
             result = Calculator.CalculatePair(5, 2, "+");
             Assert.AreEqual(result, 7);
         }
+        //Subtraction
+        [Test]
+        public void TestSubtraction()
+        {
+            result = Calculator.CalculatePair(5, 2, "-");
+            Assert.AreEqual(result, 3);
+        }
+
+        [UnityTest]
+        public IEnumerator TestUnitySubtraction()
+        {
+            //Use the Assert Class to Test Conditions
+            //Use yield to skip a frame
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "-");
+            Assert.AreEqual(result, 3);
+        }
+
+        //Multiplication
+        [Test]
+        public void TestMultiplication()
+        {
+            result = Calculator.CalculatePair(5, 2, "*");
+            Assert.AreEqual(result, 10);
+        }
+
+        [UnityTest]
+        public IEnumerator TestUnityMultiplication()
+        {
+            //Use the Assert Class to Test Conditions
+            //Use yield to skip a frame
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "*");
+            Assert.AreEqual(result, 10);
+        }
+
+        //Division
+        [Test]
+        public void TestDivision()
+        {
+            result = Calculator.CalculatePair(5, 2, "/");
+            Assert.AreEqual(result, 2.5f);
+        }
+
+        [UnityTest]
+        public IEnumerator TestUnityDivision()
+        {
+            //Use the Assert Class to Test Conditions
+            //Use yield to skip a frame
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "/");
+            Assert.AreEqual(result, 2.5f);
+        }
+
+        //Square Root
+        [Test]
+        public void TestSquareRoot()
+        {
+            result = Calculator.CalculatePair(5, 2, "Mathf.Sqrt");
+            Assert.AreEqual(result, 0.0f);
+        }
+
+        [UnityTest]
+        public IEnumerator TestUnitySquareRoot()
+        {
+            //Use the Assert Class to Test Conditions
+            //Use yield to skip a frame
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "Mathf.Sqrt");
+            Assert.AreEqual(result, 0.0f);
+        }
+
+        //Power
+        [Test]
+        public void TestPower()
+        {
+            result = Calculator.CalculatePair(5, 2, "Math.Pow");
+            Assert.AreEqual(result, 0.0f);
+        }
+
+        [UnityTest]
+        public IEnumerator TestUnityPowert()
+        {
+            //Use the Assert Class to Test Conditions
+            //Use yield to skip a frame
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "Math.Pow");
+            Assert.AreEqual(result, 0.0f);
+        }
     }
 }
